d0343cbdc861461b81bb3c95f8ea0336702dabdb
Niccolò Zuccolo <niccolo@macbookpro.home>
2020-05-11 16:05:26 +0200

    Giorno 2

---
+++ dockerbox/main.tf
@@ -0,0 +1,78 @@
+// CONFIGURAZIONE PROVIDER
+provider "google" {
+  credentials = file("key.json")
+  project     = "dockerbox"
+  region      = "us-west1"
+}
+
+// CREAZIONE DEL NETWORK
+resource "google_compute_network" "vpc_network" {
+  name = "gitlab-network"
+}
+
+// CREAZIONE IP PUBBLICO STATICO
+resource "google_compute_address" "ip_address" {
+  name = "gitlab-address"
+}
+
+// IMPOSTAZIONE DEL FIREWALL
+resource "google_compute_firewall" "default" {
+  name    = "gitlab-app-firewall"
+  network = "${google_compute_network.vpc_network.id}"
+  allow {
+    protocol = "tcp"
+    ports    = ["80","22","5050","443"]
+  }
+}
+
+// GENERATORE DI NUMERI RANDOM
+resource "random_id" "instance_id" {
+  byte_length = 2
+}
+
+// CREAZIONE DELLA ISTANZA O VM
+resource "google_compute_instance" "default" {
+  name         = "gitlab-vm-${random_id.instance_id.hex}"
+  machine_type = "n1-standard-4"
+  zone         = "us-west1-b"
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-lts"
+      size  = 64
+    }
+  }
+  metadata = {
+    ssh-keys = "niccolo.zuccolo:${file("~/.ssh/id_ed25519.pub")}"
+    startup-script = "${file("scripts/bootstrap.sh")}"
+  }
+  network_interface {
+    network = "${google_compute_network.vpc_network.id}"
+    access_config {
+      nat_ip = "${google_compute_address.ip_address.address}"
+    }
+  }
+}
+
+// STAMPA DELL'INDIRIZZO IP
+output "instance_details" {
+  value = "${google_compute_instance.default}"
+}
+
+// HEALTH CHECK TCP
+resource "google_compute_health_check" "tcp-health-check" {
+  name        = "tcp-health-check"
+  description = "Health check via tcp"
+
+  timeout_sec         = 1
+  check_interval_sec  = 1
+  healthy_threshold   = 4
+  unhealthy_threshold = 5
+
+  tcp_health_check {
+    port_name          = "health-check-port"
+    port_specification = "USE_NAMED_PORT"
+    request            = "ARE YOU HEALTHY?"
+    proxy_header       = "NONE"
+    response           = "I AM HEALTHY"
+  }
+}
---
+++ dockerbox/scripts/bootstrap.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# AGGIORNAMENTO DEI SOFTWARE
+sudo apt update -y
+sudo apt dist-upgrade -y
+
+# INSTALLAZIONE DI GITLAB
+sudo apt install curl openssh-server ca-certificates -y
+curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
+sudo apt install gitlab-ce -y
+
+# MODIFICA E CONFIGURAZIONE DEL FILE gitlab.rb
+sudo sed -i "s/http\:\/\/gitlab.example.com/https\:\/\/dockerbox.ch/g" /etc/gitlab/gitlab.rb
+sudo sed -i "s/\# letsencrypt\['enable'\] = nil/letsencrypt\['enable'\] = true/g" /etc/gitlab/gitlab.rb
+sudo sed -i "s/\# letsencrypt\['contact_emails'\] = \[\]/letsencrypt\['contact_emails'\] = \['zuccolon@cscs.ch'\]/g" /etc/gitlab/gitlab.rb
+sudo sed -i "s/\# letsencrypt\['group'\] = 'root'/letsencrypt\['group'\] = 'root'/g" /etc/gitlab/gitlab.rb
+sudo sed -i "s/\# letsencrypt\['key_size'\] = 2048/letsencrypt\['key_size'\] = 2048/g" /etc/gitlab/gitlab.rb
+sudo sed -i "s/\# letsencrypt\['owner'\] = 'root'/letsencrypt\['owner'\] = 'root'/g" /etc/gitlab/gitlab.rb
+sudo sed -i "s/\# letsencrypt\['wwwroot'\] = '\/var\/opt\/gitlab\/nginx\/www'/letsencrypt\['wwwroot'\] = '\/var\/opt\/gitlab\/nginx\/www'/g" /etc/gitlab/gitlab.rb
+sudo sed -i "s/\# letsencrypt\['auto_renew'\] = true/letsencrypt\['auto_renew'\] = true/g" /etc/gitlab/gitlab.rb
+sudo sed -i "s/\# letsencrypt\['auto_renew_hour'\] = 0/letsencrypt\['auto_renew_hour'\] = 2/g" /etc/gitlab/gitlab.rb
+sudo sed -i "s/\# letsencrypt\['auto_renew_day_of_month'\] = \"\*\/4\"/letsencrypt\['auto_renew_day_of_month'\] = \"\*\/4\"/g" /etc/gitlab/gitlab.rb
+
+# RICONFIGURAZIONE DI GITLAB
+sudo gitlab-ctl reconfigure
